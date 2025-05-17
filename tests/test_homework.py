dae8aa7e655856751b1628c2d3d7943bf4f1acbe
Macuellarmo <macuellarmo@unal.edu.co>
2025-05-17 09:07:39 -0500

    intento2

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -1,34 +1,49 @@
-"""Autograding script for student homework."""
-
 import os
 import subprocess
 
-import sys
 
-from ._internals.count_words import count_words
-from ._internals.preprocess_lines import preprocess_lines
-from ._internals.read_all_lines import read_all_lines
-from ._internals.split_into_words import split_into_words
-from ._internals.write_word_counts import write_word_counts
+def test_homework():
+    """Test Word Count"""
 
+    # Carpetas y archivos del paquete homework
 
-def main():
-    """Main function to orchestrate the word count process."""
+    for path in [
+        "homework/src",
+        "homework/src/_internals",
+        "homework/src/_internals/count_words.py",
+        "homework/src/_internals/preprocess_lines.py",
+        "homework/src/_internals/read_all_lines.py",
+        "homework/src/_internals/split_into_words.py",
+        "homework/src/_internals/write_word_counts.py",
+    ]:
+        if not os.path.exists(path):
+            raise Exception(f"'{path}' directory does not exist")
+    try:
+        subprocess.run(
+            ["python", "-m", "homework", "data/input", "data/output"],
+            check=True,
+        )
+    except subprocess.CalledProcessError as e:
+        raise Exception(f"Error running the homework script: {e}")
 
-    if len(sys.argv) != 3:
-        print("Usage: python3 -m homework <input_folder> <output_folder>")
-        sys.exit(1)
+    if not os.path.exists("data/output/"):
+        raise Exception("'data/output/' directory does not exist")
 
-    input_folder = sys.argv[1]
-    output_folder = sys.argv[2]
+    results_file = "data/output/wordcount.tsv"
+    if not os.path.exists(results_file):
+        raise Exception(f"'{results_file}' file does not exist")
 
-    lines = read_all_lines(input_folder)
-    preprocessed_lines = preprocess_lines(lines)
-    words = split_into_words(preprocessed_lines)
-    word_counts = count_words(words)
-    write_word_counts(output_folder, word_counts)
+    with open(results_file, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+        result = {}
+        for line in lines:
+            key, value = line.strip().split("\t")
+            result[key] = int(value)
 
+    assert result.get("analytics", 0) == 5, "Incorrect count for 'analytics'"
+    assert result.get("business", 0) == 7, "Incorrect count for 'business'"
+    assert result.get("by", 0) == 3, "Incorrect count for 'by'"
+    assert result.get("algorithms", 0) == 2, "Incorrect count for 'algorithms'"
+    assert result.get("analysis", 0) == 4, "Incorrect count for 'analysis'"
 
-if __name__ == "__main__":
-    main()
-
+    
